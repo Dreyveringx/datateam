7ba77adc1263cc8fec74719f335e4fdd92b88c15
Jair De Jesus Uribe Valencia <143142760+Dreyveringx@users.noreply.github.com>
2025-01-29 15:36:58 -0500

    Create Entity User

---
+++ Backend/src/main/java/com/demoDatateam/dataTeam/domain/model/user.java
@@ -0,0 +1,32 @@
+package com.demoDatateam.dataTeam.domain.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_document_type")
+    private Integer idDocumentType;
+
+    @Column(name = "document_number")
+    private Integer documentNumber;
+
+    private String password;
+    
+
+}
