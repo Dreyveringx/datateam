53b898627fd3679e1217ce54929dc3e0b6bff95e
Jair De Jesus Uribe Valencia <143142760+Dreyveringx@users.noreply.github.com>
2025-01-30 09:42:31 -0500

    Update UserRepositoryPort

--- Backend/src/main/java/com/demoDatateam/dataTeam/domain/port/UserRepositoryPort.java
+++ Backend/src/main/java/com/demoDatateam/dataTeam/domain/port/UserRepositoryPort.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import com.demoDatateam.dataTeam.domain.model.User;
 
 public interface UserRepositoryPort {
-    Optional<User>findByUsername(String username);
+    Optional<User>findByDocumentNumber(Integer documentNumber);
     User save(User user);
 
 }
