7f413651ea9ec8c0be43319380ce5f296d6c3ed4
MancipeManuel <162451732+MancipeManuel@users.noreply.github.com>
2025-01-30 10:49:09 -0500

    Update UserService.java

--- Backend/src/main/java/com/demoDatateam/dataTeam/application/service/UserService.java
+++ Backend/src/main/java/com/demoDatateam/dataTeam/application/service/UserService.java
@@ -1,6 +1,31 @@
 package com.demoDatateam.dataTeam.application.service;
 
-public class UserService {
-    
+import com.demoDatateam.dataTeam.domain.port.DocumentTypePort;
+import com.demoDatateam.dataTeam.domain.port.UserDocumentNumberPort;
+import com.demoDatateam.dataTeam.domain.port.UserPasswordPort;
 
+public class UserService {
+    private final DocumentTypePort documentTypePort;
+    private final UserDocumentNumberPort userDocumentNumberPort;
+    private final UserPasswordPort userPasswordPort;
+
+    public UserService(
+            DocumentTypePort documentTypePort,
+            UserDocumentNumberPort userDocumentNumberPort,
+            UserPasswordPort userPasswordPort) { 
+        this.documentTypePort = documentTypePort;
+        this.userDocumentNumberPort = userDocumentNumberPort;
+        this.userPasswordPort = userPasswordPort;
+    }
+
+    public boolean authenticateUser(String documentType, String documentNumber, String userPassword) {
+        String type = documentTypePort.getDocumentType(documentType);
+        boolean documentValid = userDocumentNumberPort.validateDocumentNumber(type, documentNumber);
+
+        if (documentValid) {
+            String username = type + "_" + documentNumber;
+            return userPasswordPort.authenticatePassword(username, userPassword);
+        }
+        return false;
+    }
 }
