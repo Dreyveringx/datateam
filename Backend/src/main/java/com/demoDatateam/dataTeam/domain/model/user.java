dcaee2831a65c808541705aa77653a2d1b8ff994
MancipeManuel <162451732+MancipeManuel@users.noreply.github.com>
2025-01-30 09:22:14 -0500

    modelo user

--- Backend/src/main/java/com/demoDatateam/dataTeam/domain/model/user.java
+++ Backend/src/main/java/com/demoDatateam/dataTeam/domain/model/user.java
@@ -1,5 +1,14 @@
 package com.demoDatateam.dataTeam.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class User {
+    private Long id;
+    private Integer idDocumentType;
+    private Integer documentNumber;
+    private String password;
 
 }
