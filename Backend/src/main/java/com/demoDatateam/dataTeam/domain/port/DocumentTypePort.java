f9b0a3c45660006a16ef21076758b58ef256617b
Jair De Jesus Uribe Valencia <143142760+Dreyveringx@users.noreply.github.com>
2025-01-30 10:12:37 -0500

    Create DocumentTypePort and Create UserDocumentNumberPort

---
+++ Backend/src/main/java/com/demoDatateam/dataTeam/domain/port/DocumentTypePort.java
@@ -0,0 +1,6 @@
+package com.demoDatateam.dataTeam.domain.port;
+
+public interface DocumentTypePort {
+
+    String getDocumentType (String type);
+}
---
+++ Backend/src/main/java/com/demoDatateam/dataTeam/domain/port/UserDocumentNumberPort.java
@@ -0,0 +1,5 @@
+package com.demoDatateam.dataTeam.domain.port;
+
+public interface UserDocumentNumberPort {
+    boolean validateDocumentNumber (String documentType, String documentNumber );
+}
