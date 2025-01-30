a7afdf499b91378555203532167d5c090b2b3e99
Jair De Jesus Uribe Valencia <143142760+Dreyveringx@users.noreply.github.com>
2025-01-30 16:44:56 -0500

    userRepository

--- Backend/src/main/java/com/demoDatateam/dataTeam/infrastructure/adapter/persistence/userRepository.java
+++ Backend/src/main/java/com/demoDatateam/dataTeam/infrastructure/adapter/persistence/userRepository.java
@@ -1,10 +1,9 @@
 package com.demoDatateam.dataTeam.infrastructure.adapter.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.demoDatateam.dataTeam.domain.model.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUserDocumentTypeAndUserDocumentNumber(String userDocumentType, Integer userDocumentNumber);
-} 
+    User findByDocumentTypeAndDocumentNumber(Integer documentType, Integer documentNumber);
+}
